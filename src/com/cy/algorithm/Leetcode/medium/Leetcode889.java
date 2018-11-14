0f157ed174ecac1cf64311ad2e9ac59e0abaac33
cao-yan <clay@github.com>
2018-11-14 19:22:04 +0800

    2018-11-14

---
+++ src/com/cy/algorithm/Leetcode/medium/Leetcode889.java
@@ -0,0 +1,67 @@
+package com.cy.algorithm.Leetcode.medium;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 题名 : 根据前序和后序遍历构造二叉树
+ * 题目 : 返回与给定的前序和后序遍历匹配的任何二叉树。
+ *  pre 和 post 遍历中的值是不同的正整数。
+ *
+ *  思路 : pre[0] = post[N-1],pre[1]是左子树头结点,post[N-2]是右子树头结点,找到分割点递归
+ * 执行时间 : 15ms
+ * @author clay
+ * @date 18-11-14 14:57
+ */
+public class Leetcode889 {
+
+
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+     }
+
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        int N = pre.length;
+        if(N == 0){
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[0]);
+        if(N == 1){
+            return root;
+        }
+        int l = -1;
+        for(int i = 0; i < N; i++){
+            if(post[i] == pre[1]){
+                l = i;
+            }
+        }
+        root.left = constructFromPrePost(Arrays.copyOfRange(pre,1,l+2),
+                Arrays.copyOfRange(post,0,l+1));
+        root.right = constructFromPrePost(Arrays.copyOfRange(pre,l+2,N),
+                Arrays.copyOfRange(post,l+1,N-1));
+        return root;
+    }
+
+    public static void main(String []args){
+        Leetcode889 l = new Leetcode889();
+        TreeNode root = l.constructFromPrePost(new int[]{1,2,4,5,3,6,7},
+                new int[]{4,5,2,6,7,3,1});
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(node == null){
+                continue;
+            }
+            queue.offer(node.left);
+            queue.offer(node.right);
+            System.out.print("" + node.val + " ");
+
+        }
+    }
+
+}
---
+++ src/com/cy/algorithm/Leetcode/medium/Leetcode901.java
@@ -0,0 +1,59 @@
+package com.cy.algorithm.Leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 题名 : 股票价格跨度
+ * 题目 : 编写一个 StockSpanner 类，它收集某些股票的每日报价，并返回该股票当日价格的跨度。
+ * 今天股票价格的跨度被定义为股票价格小于或等于今天价格的最大连续日数（从今天开始往回数，包括今天）。
+ * 例如，如果未来7天股票的价格是 [100, 80, 60, 70, 60, 75, 85]，
+ * 那么股票跨度将是 [1, 1, 1, 2, 1, 4, 6]。
+ *
+ * 思路 : 用数组记录每一天的price,直接倒着遍历会超时,需要把之前每一天的价格跨度保存起来,往前跳跃遍历
+ * 执行时间 : 122ms
+ * @author clay
+ * @date 18-11-14 17:21
+ */
+public class Leetcode901 {
+
+    class Stock{
+        int price;
+        int len;
+
+        public Stock(int price, int len) {
+            this.price = price;
+            this.len = len;
+        }
+    }
+
+    private ArrayList<Stock> list = new ArrayList<>(100001);
+
+    private int cur = -1;
+
+    public int next(int price) {
+        int result = 1;
+        int i = cur;
+        Stock stock;
+        while(i >=0 && (stock = list.get(i)).price <= price){
+            result += stock.len;
+            i -= stock.len;
+        }
+        list.add(new Stock(price,result));
+        cur++;
+        return result;
+    }
+
+    public static void main(String ...args){
+//        Leetcode901 l = new Leetcode901();
+//        int []arr = new int[]{100,80,60,70,60,75,85};
+//        for(int i : arr){
+//            System.out.println(l.next(i));
+//        }
+        List<Integer> list = new ArrayList<>(1000);
+        System.out.println(list.size());
+
+    }
+}
