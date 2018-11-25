2397e3f1ecb5bcb9a05e8eb2a54e98f1c3886b16
cao-yan <clay@github.com>
2018-11-25 21:06:31 +0800

    2018-11-25

--- src/com/cy/algorithm/leetcode/medium/Leetcode$939.java
+++ src/com/cy/algorithm/leetcode/medium/Leetcode$939.java
@@ -4,8 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * description
+ * 题名 : 最小面积矩形
+ * 题目 : 给定在 xy 平面上的一组点，确定由这些点组成的矩形的最小面积，其中矩形的边平行于 x 轴和 y 轴。
+ * 如果没有任何矩形，就返回 0。
  *
+ * 思路 : 把每个点加入set任取两个点,判断是否存在另外两个点,保存当前最小面积
  * @author clay
  * @date 18-11-23 10:05
  */
